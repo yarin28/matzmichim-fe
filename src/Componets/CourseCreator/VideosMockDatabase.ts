6f0b2983dd4d5792b723b683845bd87145dea1fc
Yarin <yarin.greenfeld@gmail.com>
2022-03-07 17:10:43 +0200

    moved the videos database to better place

---
+++ src/Componets/CourseCreator/VideosMockDatabase.ts
@@ -0,0 +1,116 @@
+    const MockVideosDatabase = [
+        {
+            "uid" : "082AC3D7-DB20-4727-AEA1-34E3F7159E38",
+            "title": "מצמיחים - מי אנחנו",
+            "url": "https://www.youtube.com/watch?v=6xAd-lbfQgg",
+            "type": "youtube",
+            "language": "hebrew"
+        },
+        {
+            "uid" : "0819C5C6-E6ED-4F85-AFAB-06071744F072",
+            "title": "גיל - בתוך העולם הסודי של בני הנוער",
+            "url": "https://photos.app.goo.gl/QR87TPeb1kBXJPyt6",
+            "type": "google photos",
+            "language": "hebrew"
+        
+        },
+        {
+            "uid" : "8C969558-5E5C-4BD6-881E-2BE0F165AF16",
+            "title": " הרצאת TED של דר פול פיף -האם הכסף הופך אותנו לרעים?  ",
+            "url": "https://www.ted.com/talks/paul_piff_does_money_make_you_mean/transcript?language=he&hc_location=ufi#t-9894",
+            "type": "ted",
+            "language": "hebrew"
+        },
+        {
+            "uid" : "ED5DCB3D-3817-4604-B115-05A2B1C9F5A9",
+            "title": " פופולאריות ותוקפנות מכשירנית - הרצאה של פרופסור רוברט פאריס",
+            "url": "https://photos.app.goo.gl/PgXXfivNeTb5uQcR6",
+            "type": "google photos",
+            "language": "hebrew"
+        },
+        {
+            "uid" : "92123E63-216D-4033-9A55-32E8ECAF49E1",
+            "title": " הרצאה של פרופ' אליזבת פאלוק (בטסי) כיצד נורמות קולקטיביות יכולות לייצר או לתווך אקלים של סכסוך    ",
+            "url": "https://photos.app.goo.gl/25w5PcrhuvnVsj6p8",
+            "type": "google photos",
+            "language": "hebrew"
+        },
+        {
+            "uid" : "24416904-9CD9-470A-ADC9-5A4AEA00F151",
+            "title": " הרצאת TED של פרופסור מיכאל נורטון- איך לקנות אושר",
+            "url": "https://www.youtube.com/watch?v=oqUKmHULpd4",
+            "type": "youtube",
+            "language": "hebrew"
+        },
+        {
+            "uid" : "B131D012-75FE-4A79-A8EB-D6EF884E115D",
+            "title": "ירידה מפתיעה באלימות",
+            "url": "https://www.ted.com/talks/steven_pinker_the_surprising_decline_in_violence?language=he#t-7614",
+            "type": "ted",
+            "language": "hebrew"
+        },
+        {
+            "uid" : "D19063D6-8340-4401-9CE7-01F87471CD68",
+            "title": "האם העולם משתפר או מדרדר? מבט על המספרים",
+            "url": "https://www.ted.com/talks/steven_pinker_is_the_world_getting_better_or_worse_a_look_at_the_numbers?language=en",
+            "type": "ted",
+            "language": "hebrew"
+        },
+        {
+            "uid" : "F2431F58-4413-4072-B251-215E5C4E3A2D",
+            "title": "איך להתחיל תנועה - הרצאת טד של דרק סיברס",
+            "url": "https://www.ted.com/talks/derek_sivers_how_to_start_a_movement?language=he#t-10251",
+            "type": "ted",
+            "language": "hebrew"
+        },
+        {
+            "uid" : "316D8EEE-8C04-4414-B964-6D731B89ADB5",
+            "title": "שקט- כוחם של המופנמים בעולם שלא מפסיק לדבר- הרצאת TED של סוזן קיין",
+            "url": "https://www.ted.com/talks/susan_cain_the_power_of_introverts?language=he#t-5187",
+            "type": "ted",
+            "language": "hebrew"
+        },
+        {
+            "uid" : "144F8D8E-65D1-4156-B4DA-B5143CD66950",
+            "title": "סליחה על השאלה - חרם ובריונות",
+            "url": "https://www.youtube.com/watch?v=xVExqI1jHG0",
+            "type": "youtube",
+            "language": "hebrew"
+        },
+        {
+            "uid" : "66DE0832-22AE-459B-9A43-25C41CDD02F1",
+            "title": "ירידה מפתיעה באלימות",
+            "url": "https://www.ted.com/talks/steven_pinker_the_surprising_decline_in_violence?language=en",
+            "type": "ted",
+            "language": "hebrew"
+        },
+        {
+            "uid" : "36120902-5BD2-4D5A-8DD8-ADE83B0E4F62",
+            "title": "האם העולם משתפר או מדרדר? מבט על המספרים",
+            "url": "https://www.ted.com/talks/steven_pinker_is_the_world_getting_better_or_worse_a_look_at_the_numbers?language=en",
+            "type": "ted",
+            "language": "hebrew"
+        },
+        {
+            "uid" : "951C4209-E02B-4B29-B766-D6E7E8D95BEA",
+            "title": "השפעה חברתית- קונפורמיות - ציות למוסכמות בקבוצה ",
+            "url": "https://www.youtube.com/watch?v=dOHzv3WAXxo&ab_channel=%D7%91%D7%A8%D7%A7%D7%A4%D7%99%D7%9E%D7%94-%D7%9E%D7%A0%D7%97%D7%94%D7%A7%D7%91%D7%95%D7%A6%D7%95%D7%AA%2C%D7%9E%D7%90%D7%9E%D7%9F%D7%90%D7%99%D7%A9%D7%99%D7%95%D7%9E%D7%A8%D7%A6%D7%94",
+            "type": "youtube",
+            "language": "hebrew"
+        },
+        {
+            "uid" : "BDE6A282-55E3-4367-9640-272DD917FBA9",
+            "title": "סגנונות הנחיה- להנהיג כמו המנצחים הדגולים",
+            "url": "https://www.ted.com/talks/itay_talgam_lead_like_the_great_conductors?language=he#t-125850 ",
+            "type": "ted",
+            "language": "hebrew"
+        },
+        {
+            "uid" : "4D810F3D-4D55-47D7-A959-60C79BD2A563",
+            "title": "כל האמת בפרצוף - אשקלון, ילדים יוצרים סרטים",
+            "url": "https://youtu.be/f7qY_5UKB7E",
+            "type": "youtube",
+            "language": "hebrew"
+        }
+    ];
+    export default MockVideosDatabase;
