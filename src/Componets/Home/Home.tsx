e32af713a539b924a60e3b492c2d37c729f995ab
Yarin <yarin.greenfeld@gmail.com>
2022-03-27 01:43:32 +0300

    now the home page is in heb

--- src/Componets/Home/Home.tsx
+++ src/Componets/Home/Home.tsx
@@ -2,6 +2,9 @@ import { Typography } from "@mui/material"
 import React, { ComponentType, useCallback, useEffect, useRef, useState } from "react"
 const Home: ComponentType = (props) => {
     return(
-        <Typography variant="h1" > this is the home page for the app</Typography>
+        <>
+        {/* <Typography variant="h1" > this is the home page for the app</Typography> */}
+        <Typography variant="h1" dir="rtl" >  ברוכים הבאים להשתלמות של עמותת מצמיחים! </Typography>
+        </>
     )}
     export default Home;
