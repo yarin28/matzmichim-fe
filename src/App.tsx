d392455f5762953c82bbf3ad36fe83e3e228dd21
Yarin <yarin.greenfeld@gmail.com>
2022-03-27 01:43:48 +0300

    added the pathes

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,22 @@ import Home from './Componets/Home';
 import VideoCard from './Componets/VideoCard';
 import VideoCreator from './Componets/VideoCreator';
 import CourseCreator from './Componets/CourseCreator';
+import Test from './Componets/Test';
+import CommentList from './Componets/CommentList';
+import { CssBaseline } from '@mui/material';
 
 const App:ComponentType=()=> {
   return (
     <> 
+    <CssBaseline />
      <Router>
       <Routes>
         <Route path="/" element={<Home/>}  />
         <Route path="VideoCard" element={<VideoCard type="youtube" title="איך לקנות אושר - מיכאל נורטון Michael Norton" uid="oqUKmHULpd4" description='this is the video description/ the short summry/notes' url="https://www.youtube.com/watch?v=oqUKmHULpd4" />}  />
         <Route path="VideoCreator"element={<VideoCreator/>}  />
         <Route path="CourseCreator"element={<CourseCreator/>}  />
+        <Route path="Test"element={<Test/>}  />
+        <Route path="CommentList"element={<CommentList/>}  />
       </Routes>
 </Router>
 </>
