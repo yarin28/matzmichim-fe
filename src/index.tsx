a93bd03a1717f98ee2d19e4f0b85c47aee1ccd2e
Yarin <yarin.greenfeld@gmail.com>
2022-06-22 01:06:07 +0300

    added translation

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import i18n from "i18next"
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector'
+import HttpApi from 'i18next-http-backend'
+
+i18n.use(initReactI18next).use(LanguageDetector).use(HttpApi).init({
+  
+  supportedLngs:['en','he'],
+  fallbackLng:"en",
+  detection:{
+  order: ['querystring', 'cookie', 'localStorage', 'sessionStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
+    caches:['cookie'],
+  lookupFromPathIndex: 0,
+  },
+  backend:{
+    loadPath:'assets/locales/{{lng}}/translations.json',
+  },
+  react:{
+    useSuspense:false
+  },
+
+});
 ReactDOM.render(
     <App />,
   document.getElementById('root')
