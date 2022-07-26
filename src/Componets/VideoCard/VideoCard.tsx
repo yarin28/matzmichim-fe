01b991a5a41786986a85cf27afae7f95efffd005
ronrk <ronrk11@gmail.com>
2022-07-26 19:34:32 +0300

    Ron TEST

--- src/Componets/VideoCard/VideoCard.tsx
+++ src/Componets/VideoCard/VideoCard.tsx
@@ -1,20 +1,26 @@
-import React, { ComponentType, } from "react"
-import { Avatar, Card, CardContent, CardHeader, Typography } from "@mui/material";
+import React, { ComponentType } from 'react';
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  Typography,
+} from '@mui/material';
 import { red } from '@mui/material/colors';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 interface VideoCardProps {
-  title: string;//could name it "name"
+  title: string; //could name it "name"
   description?: string;
   length?: number;
   url: string;
-  language?: "English" | "Hebrew" | "Arabic" | undefined;
-  type: "ted" | "youtube" | "google photos" | undefined;
+  language?: 'English' | 'Hebrew' | 'Arabic' | undefined;
+  type: 'ted' | 'youtube' | 'google photos' | undefined;
   uid?: string;
 }
 const VideoCard: ComponentType<VideoCardProps> = (props) => {
   const embedIdFromUrl = (url: string) => {
-    const urlParts = url.split("/");
+    const urlParts = url.split('/');
     return urlParts[urlParts.length - 1];
   };
   return (
@@ -30,23 +36,23 @@ const VideoCard: ComponentType<VideoCardProps> = (props) => {
           subheader={props.description}
         />
         <CardContent>
- <iframe
-      width="300"
-      height="200"
-      src={`https://www.youtube.com/embed/${embedIdFromUrl(props.url)}`}
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      title="Embedded youtube"
-    />
-          <b>language</b> - {props?.language ? props.language : "non in db"}<br />
-          <b>length</b> - {props?.length ? props.length : "non in db"}<br />
-          <b>uid</b> -  {props?.uid}
+          <iframe
+            width="300"
+            height="200"
+            src={`https://www.youtube.com/embed/${embedIdFromUrl(props.url)}`}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="Embedded youtube"
+          />
+          <b>language</b> - {props?.language ? props.language : 'non in db'}
+          <br />
+          <b>length</b> - {props?.length ? props.length : 'non in db'}
+          <br />
+          <b>uid</b> - {props?.uid}
         </CardContent>
-
       </Card>
-
-
-    </>);
+    </>
+  );
 };
 export default VideoCard;
